3ee9925ee23d83aa08191ef0fe29dc7e5c3fe232
dparker1005 <dlparker1005@gmail.com>
2021-07-13 15:38:18 -0400

    Confirmation messages are now editable.

--- js/pmprogl-admin.js
+++ js/pmprogl-admin.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function () {
 	function pmprogl_update_field_visibility() {
 		if (jQuery('#pmprogl_gift_level').val() == '0') {
-			jQuery('.pmprogl_gift_level_warning').hide();
+			jQuery('.pmprogl_gift_level_toggle_setting').hide();
 			jQuery('#pmprogl_gift_expires_tr').hide();
 			jQuery('#pmprogl_period_tr').hide();
 		} else if (!jQuery('#pmprogl_gift_expires').is(':checked')) {
-			jQuery('.pmprogl_gift_level_warning').show();
+			jQuery('.pmprogl_gift_level_toggle_setting').show();
 			jQuery('#pmprogl_gift_expires_tr').show();
 			jQuery('#pmprogl_period_tr').hide();
 		} else {
-			jQuery('.pmprogl_gift_level_warning').show();
+			jQuery('.pmprogl_gift_level_toggle_setting').show();
 			jQuery('#pmprogl_gift_expires_tr').show();
 			jQuery('#pmprogl_period_tr').show();
 		}
